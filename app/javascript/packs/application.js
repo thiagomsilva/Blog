3a1f9a5ed08d050a6b88338d189fcc643f416a85
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2022-12-23 11:14:08 -0300

    add vizualização de comentários com data e hora

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,10 @@ import "../src/application.css";
 
 Rails.start()
 ActiveStorage.start()
+
+// const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+// const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
